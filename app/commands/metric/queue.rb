dc6d16e1c4ae58c9a2718ce5cbd39239357486b9
Erik Schierboom <erik_schierboom@hotmail.com>
2022-07-14 15:45:11 +0200

    Use country_code when creating metric

--- app/commands/metric/queue.rb
+++ app/commands/metric/queue.rb
@@ -1,10 +1,10 @@
 class Metric::Queue
   include Mandate
 
-  initialize_with :type, :occurred_at, :attributes
+  initialize_with :type, :occurred_at, :country_code, :attributes
 
   def call
-    LogMetricJob.perform_later(type, occurred_at, **attributes)
+    LogMetricJob.perform_later(type, occurred_at, country_code, **attributes)
   rescue StandardError => e
     # Don't crash if the creation fails, e.g. if sidekiq is down
     Bugsnag.notify(e)
